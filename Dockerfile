594b724b3c0aabdb0351174febe779b93289f708
Peter Stadler <stadlerpeter@yahoo.fr>
2018-05-16 23:43:24 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM httpd:2.4
+LABEL maintainer="Peter Stadler for the ViFE"
+
+# For information about these parameters see 
+# https://httpd.apache.org/docs/2.4/mod/mod_authnz_ldap.html
+ARG AuthLDAPURL
+ARG AuthLDAPBindDN
+ARG AuthLDAPBindPassword
+ARG RequireLDAPGroup
+
+COPY entrypoint.sh /my-docker-entrypoint.sh
+RUN chmod 755 /my-docker-entrypoint.sh
+
+WORKDIR /usr/local/apache2
+RUN echo "Include conf/extra/vife.conf" >> conf/httpd.conf
+
+VOLUME ["/var/svn"]
+ENTRYPOINT ["/my-docker-entrypoint.sh"]
+CMD ["httpd-foreground"]
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# add our dav location to the httpd config
+cat <<EOF > /usr/local/apache2/conf/extra/vife.conf
+LoadModule	dav_module           modules/mod_dav.so
+LoadModule  dav_fs_module        modules/mod_dav_fs.so
+LoadModule	ldap_module          modules/mod_ldap.so
+LoadModule	authnz_ldap_module   modules/mod_authnz_ldap.so
+DocumentRoot "/var/webdav"
+<Directory "/var/webdav">
+    DAV on
+    Options +Indexes
+    AllowOverride None
+    AuthName "ViFE WebDAV: Login mit Nutzernamen und Passwort" 
+    AuthBasicProvider ldap
+    AuthType Basic
+    AuthLDAPGroupAttribute member
+    AuthLDAPGroupAttributeIsDN on
+    AuthLDAPURL ${AuthLDAPURL}
+    AuthLDAPBindDN "${AuthLDAPBindDN}" 
+    AuthLDAPBindPassword "${AuthLDAPBindPassword}" 
+    Require ldap-group CN=${RequireLDAPGroup},CN=Users,DC=muwi,DC=hfm-detmold,DC=de
+</Directory>
+EOF
+
+# run the command given in the Dockerfile at CMD 
+exec "$@"
